90d8076cdb1f606c1a2905a87e622f4ffd4e1fe8
Ryan Alexander <scloopy@gmail.com>
2012-03-04 13:43:37 -0800

    Clean up selection scroll code

--- main.js
+++ main.js
@@ -64,15 +64,14 @@ function(core, material, event, selector ){
         code_text.addEventListener("keypress", function(e){
             e.stopPropagation();
         }, false);
-        code_text.addEventListener("mousewheel", function(e) {
 
-            // magic selector
+        // Magic Selector
+        code_text.addEventListener("mousewheel", function(e){
             var wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 40;
-            var selected = selector.get_selection(code_text.id);
+            var selected = selector.getSelection(code_text.id);
 
             // if selection is a number, adjust value in selection via mousewheel
-            if (!isNaN (selected.text-0) && selected.text != '') {
-                
+            if(!isNaN(selected.text - 0) && selected.text != ""){
                 e.preventDefault();
                 e.stopPropagation();
 
@@ -80,24 +79,24 @@ function(core, material, event, selector ){
                 var front = orig.substr(0, selected.start);
                 var end = orig.substr(selected.end, orig.length);
                 var newValue = null;
-                if ( (selected.text+"").split('.').length == 1 ) { // if is int
+
+                if((selected.text + "").split('.').length == 1){ // if is int
                     newValue = Math.round( parseFloat(selected.text) + wheelData );
                 }
-                else if ( (selected.text+"").split('.').length == 2 ) { // it's a float
-                    var fvar = (selected.text+"").split('.');
-                    var precisionLength = (fvar[1] + '').length;
+                else if((selected.text + "").split(".").length == 2){ // it's a float
+                    var fvar = (selected.text+"").split(".");
+                    var precisionLength = (fvar[1] + "").length;
                     newValue = parseFloat(parseFloat(selected.text) + wheelData).toFixed( precisionLength );
                 }
 
-                if (newValue !== null) {
+                if(newValue !== null){
                     code_text.value = front + newValue + end;
-                    selector.set_selection(code_text.id, selected.start, selected.start + (newValue+"").length );
+                    selector.setSelection(code_text.id, selected.start, selected.start + (newValue + "").length );
                     tryCompile();
                 }
-                
+
                 return false;
             }
-
         }, false);
 
         // Drag and drop mp3
